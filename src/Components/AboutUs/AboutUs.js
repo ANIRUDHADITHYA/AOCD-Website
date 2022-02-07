a3fd550fbcfcfd0998608a43e980f360e9d9ad67
ANIRUDH ADIHYA B S <anirudhadithya.b.s@gmail.com>
2022-02-07 16:21:54 +0530

    Changed Lottie Image Style

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -22,6 +22,7 @@ function AboutUs(){
             <div class="about-col">
             
             <Player
+            id="img"
                 autoplay
                 loop
                 src="https://assets3.lottiefiles.com/packages/lf20_0bzu9jvt.json"
