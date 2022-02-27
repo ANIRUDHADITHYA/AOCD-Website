505825b16b14e06dc08346643563fb6ebdf00402
ANIRUDH ADIHYA B S <anirudhadithya.b.s@gmail.com>
2022-02-27 14:21:24 +0530

    Summary Updated

--- src/Components/Compounds/ShowLess/ShowLess.js
+++ src/Components/Compounds/ShowLess/ShowLess.js
@@ -1,12 +1,15 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+import ShowSummary from "../ShowSummary/ShowSummary";
 import './ShowLess.css';
+import { BrowserRouter } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 const ShowLess = (props) => {
 
     const onSubmit = async (event) => {
 		event.preventDefault();
-		window.location = "/summary/" + props.index;
+		window.location.href = "/summary/" + props.index;
 	};
-
     return(
         <div>
             {
@@ -20,7 +23,7 @@ const ShowLess = (props) => {
                         <div className='compound-details'><h4>Origin: </h4><p>{props.compound.origin}</p></div>
                         <div className='compound-details'><h4>IUPAC Name: </h4><p>{props.compound.iupac_name}</p></div>
                         <div className='compound-details'><h4>Canonical Smiles: </h4><p>{props.compound.canl_smiles}</p></div>
-                        <div className="compound-footer" onClick={onSubmit}>Summary</div>
+                        <div className="compound-footer" id="submit" onClick={onSubmit}>Summary</div>
                     </div>) : (props.filter===2) && (props.compound.filter_id==='AS' || props.compound.filter_id==='PL_AS_AG' || props.compound.filter_id==='PL_AS' || props.compound.filter_id==='AS_AG') ? (
                     <div className="showless-container">
                         <div className='compound-name'><h3>{props.index+1}. Compound Name: {props.compound.compound_name}</h3></div>
@@ -31,7 +34,7 @@ const ShowLess = (props) => {
                         <div className='compound-details'><h4>Origin: </h4><p>{props.compound.origin}</p></div>
                         <div className='compound-details'><h4>IUPAC Name: </h4><p>{props.compound.iupac_name}</p></div>
                         <div className='compound-details'><h4>Canonical Smiles: </h4><p>{props.compound.canl_smiles}</p></div>
-                        <div className="compound-footer" onClick={onSubmit}>Summary</div>
+                        <div className="compound-footer" id="submit" onClick={onSubmit}>Summary</div>
                     </div>) : (props.filter===3) && (props.compound.filter_id==='AG' || props.compound.filter_id==='PL_AS_AG' || props.compound.filter_id==='PL_AG' || props.compound.filter_id==='AS_AG') ? (
                     <div className="showless-container">
                         <div className='compound-name'><h3>{props.index+1}. Compound Name: {props.compound.compound_name}</h3></div>
@@ -42,7 +45,7 @@ const ShowLess = (props) => {
                         <div className='compound-details'><h4>Origin: </h4><p>{props.compound.origin}</p></div>
                         <div className='compound-details'><h4>IUPAC Name: </h4><p>{props.compound.iupac_name}</p></div>
                         <div className='compound-details'><h4>Canonical Smiles: </h4><p>{props.compound.canl_smiles}</p></div>
-                        <div className="compound-footer" onClick={onSubmit}>Summary</div>
+                        <div className="compound-footer" id="submit" onClick={onSubmit}>Summary</div>
                     </div>) : (props.filter===0) ? (
                     <div className="showless-container">
                         <div className='compound-name'><h3>{props.index+1}. Compound Name: {props.compound.compound_name}</h3></div>
@@ -53,7 +56,7 @@ const ShowLess = (props) => {
                         <div className='compound-details'><h4>Origin: </h4><p>{props.compound.origin}</p></div>
                         <div className='compound-details'><h4>IUPAC Name: </h4><p>{props.compound.iupac_name}</p></div>
                         <div className='compound-details'><h4>Canonical Smiles: </h4><p>{props.compound.canl_smiles}</p></div>
-                        <div className="compound-footer" onClick={onSubmit}>Summary</div>
+                        <div className="compound-footer" id="submit" onClick={onSubmit}>Summary</div>
                     </div>) : (<div></div>)
             }
         </div>
